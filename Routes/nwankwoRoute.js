ca17586a70bb7d4ca762cd38fdba33f5bd0e99b7
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-08-12 14:48:45 +0100

    third update

--- Routes/nwankwoRoute.js
+++ Routes/nwankwoRoute.js
@@ -12,8 +12,3 @@ router.post("/", Create);
 router.delete("/:id", findByIdAndDelete);
 
 
-//module.exports = router;
-//MONGO_URL="mongodb://localhost:27017/Nmelonye"
-//srv-d25100c9c44c73b3f640
-//https://nmelonye-backend-3.onrender.com
-//102.90.100.98/32
