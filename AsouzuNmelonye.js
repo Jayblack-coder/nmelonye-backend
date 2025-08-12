4f7a09e70e3688f262f8ef551627d3aa243240aa
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-08-12 14:41:23 +0100

    update second

--- AsouzuNmelonye.js
+++ AsouzuNmelonye.js
@@ -9,18 +9,7 @@ surname:{
     type: String,
     required: true
 },
-userName:{
-    type: String,
-    required: true
-},
-password:{
-    type: Number,
-    required: true
-},
-phoneNumber:{
-    type: Number,
-    required: true
-},
+
 status:{
     type: String,
     required: true
@@ -58,7 +47,3 @@ offspring:{
 const asouzuNmelonye = mongoose.model('asouzunmelonye', asouzuNmelonyeSchema)
 module.exports = asouzuNmelonye
 
-// NEW CONNECTION STRING for my cluster
-// mongodb+srv://Ejike:<db_password>@cluster0.z2aji.mongodb.net/
-//mongodb+srv://Ejike:plot223@cluster0.z2aji.mongodb.net/
-//mongodb://localhost:27017/Nmelonye
