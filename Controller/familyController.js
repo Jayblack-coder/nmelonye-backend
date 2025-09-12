0d3d555a6cbddf887e149e6a69183048eb1bbff8
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-09-12 15:01:58 +0100

    familyController

--- Controller/familyController.js
+++ Controller/familyController.js
@@ -67,6 +67,7 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Error registering user", error: err.message });
   }
 };
+
 export const loginuser = async (req, res) => {
     try {
         const { userName, password } = req.body;
@@ -112,3 +113,72 @@ export const loginuser = async (req, res) => {
         return res.status(500).json({ message: "Error logging in", error });
     }
 };
+
+
+//GET ALL Members
+export const getAllUsers = async (req, res) => {
+    try {
+        const family = await Family.find({});
+        res.status(200).json(family)
+    } catch (error) {
+     res.status(500).json({message: error.message});   
+    }
+};
+
+
+
+
+export const  getUserById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const family = await Family.findById(id);
+        res.status(200).json(family);
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+};
+
+
+ export const findByIdAndUpdate = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const updatedfamily = await Family.findByIdAndUpdate(id, req.body);
+        if (!updatedfamily) {
+            return res.status(404).json({message:"Product not found"});
+        }
+        res.status(200).json(updatedfamily);
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+};
+
+
+export const Create = async (req, res) => {
+  try {
+    const family = await Family.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "Family member created successfully",
+      data: { family }, // model wrapped inside "data"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const findByIdAndDelete = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const family = await Family.findByIdAndDelete(id, req.body);
+        if (!family) {
+            return res.status(404).json({message:"Product not found"});
+        }
+        res.status(200).json({ message: "Product Deleted Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+}
--- Routes/familyRoute.js
+++ Routes/familyRoute.js
@@ -1,17 +1,16 @@
 import express from "express";
-import { loginuser, registerUser } from "../Controller/familyController.js";
-// import {  getFamily,getFamilyById,findByIdAndUpdate,Create,findByIdAndDelete } from "../Controller/familyController.js";
+import { loginuser, registerUser,getAllUsers,getUserById, findByIdAndUpdate, Create, findByIdAndDelete } from "../Controller/familyController.js";
+
 
 export const familyRouter = express.Router();
 
-
-// familyRouter.get("/", getFamily);
+familyRouter.get("/", getAllUsers);
 familyRouter.post('/members', registerUser);
 familyRouter.post('/login', loginuser);
-// familyRouter.get("/:id", getFamilyById);
-// familyRouter.put("/:id", findByIdAndUpdate);
-// familyRouter.post("/:id", Create);
-// familyRouter.delete("/:id", findByIdAndDelete);
+familyRouter.get("/:id", getUserById);
+familyRouter.put("/:id", findByIdAndUpdate);
+familyRouter.post("/:id", Create);
+familyRouter.delete("/:id", findByIdAndDelete);
 
 
 // module.exports = router;
