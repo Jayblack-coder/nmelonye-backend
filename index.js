38ac26d83c65cdc9c5c66930d9ca671db13bb9de
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-09-23 16:48:32 +0100

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // ✅ CORS setup
-// app.use(cors())
+
 app.use(
   cors({
     origin: [
@@ -32,6 +32,8 @@ app.use(
     credentials: true,
   })
 );
+
+
 // ✅ Handle preflight for all routes
 
 
