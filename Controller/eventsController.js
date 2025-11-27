71909963774c248b41ffb25cdcff46c83b10d58b
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-11-27 11:11:21 +0000

    eventscontroller

--- Controller/eventsController.js
+++ Controller/eventsController.js
@@ -14,18 +14,34 @@ export const createEvent = async (req, res) => {
   }
 };
 
-// export const getAllEvents = async (req, res) => {
-//   try {
-//     const events = await Event.find().sort({ date: -1 });
-//     res.status(200).json(events);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
 export const getAllEvents = async (req, res) => {
   try {
-    const events = await Event.find().sort({ date: -1 });
-    res.status(200).json({ events });
+    const events = await Event.find().sort({ date: 1 });
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updated = await Event.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updated) return res.status(404).json({ message: "Event not found" });
+
+    res.json({ message: "Event updated", event: updated });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Event.findByIdAndDelete(id);
+    if (!deleted) return res.status(404).json({ message: "Event not found" });
+
+    res.json({ message: "Event deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
