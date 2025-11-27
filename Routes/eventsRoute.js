757a1a26194e0d41004629fbe269068d73775ca4
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-11-27 11:12:24 +0000

    eventsroute

--- Routes/eventsRoute.js
+++ Routes/eventsRoute.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { createEvent, getAllEvents } from "../Controller/eventsController.js";
+import {
+  createEvent,
+  getAllEvents,
+  updateEvent,
+  deleteEvent
+} from "../Controller/eventsController.js";
 import { protect, adminOnly } from "../Middleware/authMiddleware.js";
 
 export const eventRouter = express.Router();
 
-// Public route – members can view events
+// Public route: anyone can see events
 eventRouter.get("/", getAllEvents);
 
-// Admin only – creating events
+// Admin only
 eventRouter.post("/", protect, adminOnly, createEvent);
+eventRouter.put("/:id", protect, adminOnly, updateEvent);
+eventRouter.delete("/:id", protect, adminOnly, deleteEvent);
